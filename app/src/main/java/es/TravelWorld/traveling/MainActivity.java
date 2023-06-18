ec1fb0f8d270c8f4626299a8592341b12cac8c6f
Maria <mariamolpe@hotmail.com>
2023-06-18 18:26:35 +0200

    comit init

--- app/src/main/java/es/TravelWorld/traveling/MainActivity.java
+++ app/src/main/java/es/TravelWorld/traveling/MainActivity.java
@@ -10,34 +10,36 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import es.TravelWorld.traveling.databinding.ActivityLoginBinding;
+import es.TravelWorld.traveling.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private MaterialButton btn;
     private ConstraintLayout constraintLayout;
+    private ActivityMainBinding binding;
+    private ActivityLoginBinding bindingLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        constraintLayout = (ConstraintLayout)findViewById(R.id.constraint_root);
-
-        btn = (MaterialButton) findViewById(R.id.main_button);
-        btn.setOnClickListener(v -> {
-            changeScenes(R.layout.activity_menu);
-
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        bindingLogin = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
 
 
 
     }
 
-    private void changeScenes(@LayoutRes int idLayout){
-        ConstraintSet constrainSet = new ConstraintSet();
-        constrainSet.clone(this,idLayout);
-        setContentView(idLayout);
-        constrainSet.applyTo(constraintLayout);
-
+    private void setListeners(){
+        binding.mainButton.setOnClickListener(v -> setContentView(bindingLogin.getRoot()));
+        bindingLogin.ButtonForgot.setOnClickListener(view -> Snackbar.make(view, R.string.tarde, Snackbar.LENGTH_LONG).show());
+        bindingLogin.ButtonAcount.setOnClickListener(view -> Snackbar.make(view, R.string.tarde, Snackbar.LENGTH_LONG).show());
     }
+
 
 
 }
